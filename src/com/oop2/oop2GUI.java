74322323da24e90d5ae6aee83690d0a58cfd5a92
justBoringUsername <just.boring.email@gmail.com>
2020-05-19 21:17:19 +0300

    commit

--- src/com/oop2/oop2GUI.java
+++ src/com/oop2/oop2GUI.java
@@ -7,6 +7,7 @@ import io.github.classgraph.ScanResult;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
@@ -168,14 +169,38 @@ public class oop2GUI extends JFrame {
                     String extensionFrom = fileOpen.getSelectedFile() + "";
                     int pos = extensionFrom.lastIndexOf(".");
                     extensionFrom = extensionFrom.substring(pos+1, extensionFrom.length());
+                    boolean plugInOn = false;
+                    String exSer = "";
+                    String plugName = "";
+                    for (String plugIn: plugins) {
+                        if (ex.contains(plugIn)) {
+                            plugInOn = true;
+                            plugName = plugIn;
+                            exSer = ex.replace(plugIn, "");
+                        }
+                    }
 
                     try {
-                        Class serClass = Class.forName("com.oop2.Serialize.Serialize" + ex.toUpperCase());
-                        Object serObject = serClass.newInstance();
-                        Method method1 = serClass.getMethod("setFilename", String.class);
-                        method1.invoke(serObject, fileOpen.getSelectedFile() + "");
-                        Method method2 = serClass.getMethod("putObjects", ArrayList.class);
-                        method2.invoke(serObject, elementsClone);
+                        if (plugInOn) {
+                            Class serClass = Class.forName("com.oop2.Serialize.Serialize" + exSer.toUpperCase());
+                            Object serObject = serClass.newInstance();
+                            Method method1 = serClass.getMethod("setFilename", String.class);
+                            method1.invoke(serObject, fileOpen.getSelectedFile() + "");
+                            Method method2 = serClass.getMethod("putObjects", ArrayList.class);
+                            method2.invoke(serObject, elementsClone);
+                            Class plugClass = Class.forName("com.oop2.plugins." + plugName);
+                            Object plugObject = plugClass.newInstance();
+                            Method method3 = plugClass.getMethod("encode", String.class);
+                            method3.invoke(plugObject, fileOpen.getSelectedFile() + "");
+                        }
+                        else {
+                            Class serClass = Class.forName("com.oop2.Serialize.Serialize" + ex.toUpperCase());
+                            Object serObject = serClass.newInstance();
+                            Method method1 = serClass.getMethod("setFilename", String.class);
+                            method1.invoke(serObject, fileOpen.getSelectedFile() + "");
+                            Method method2 = serClass.getMethod("putObjects", ArrayList.class);
+                            method2.invoke(serObject, elementsClone);
+                        }
                     } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
                         e.printStackTrace();
                     }
@@ -187,40 +212,83 @@ public class oop2GUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 JFileChooser fileOpen = new JFileChooser();
-                fileOpen.setAcceptAllFileFilterUsed(false);
-                for (String filterStr: getSerialize()) {
-                    FileFilterExt filterExt = new FileFilterExt("."+filterStr, "."+filterStr);
-                    fileOpen.addChoosableFileFilter(filterExt);
-                }
                 fileOpen.setDialogTitle("Save File");
                 fileOpen.setFileSelectionMode(JFileChooser.FILES_ONLY);
                 int ret = fileOpen.showDialog(null, "Open File");
                 if (ret == JFileChooser.APPROVE_OPTION) {
                     String extensionFrom = fileOpen.getSelectedFile() + "";
-                    String ex = fileOpen.getFileFilter().getDescription();
-                    ex = ex.substring(1);
                     int pos = extensionFrom.lastIndexOf(".");
                     extensionFrom = extensionFrom.substring(pos+1, extensionFrom.length());
-
-                    try {
-                        Class serClass = Class.forName("com.oop2.Serialize.Serialize" + ex.toUpperCase());
-                        Object serObject = serClass.newInstance();
-                        Method method1 = serClass.getMethod("setFilename", String.class);
-                        method1.invoke(serObject, fileOpen.getSelectedFile() + "");
-                        Method method2 = serClass.getMethod("getObjects");
-                        elementsNew = (ArrayList<Object>) method2.invoke(serObject);
-                        for (Object newObj: elementsNew) {
-                            Class carClass = newObj.getClass();
-                            idInfo = carClass.getName() + "|globalId: " +
-                                    Integer.toString(globalId);
-                            elements.put(idInfo, newObj);
-                            elementsClone.add(newObj);
-                            comboBox1.addItem(idInfo);
-
-                            globalId++;
+                    String ex = extensionFrom;
+                    boolean plugInOn = false;
+                    String exSer = "";
+                    String plugName = "";
+                    for (String plugIn: plugins) {
+                        if (ex.contains(plugIn)) {
+                            plugInOn = true;
+                            plugName = plugIn;
+                            exSer = ex.replace(plugIn, "");
                         }
-                    } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
-                        e.printStackTrace();
+                    }
+
+                    boolean plugError = false;
+                    if (plugins.isEmpty())
+                        plugError = true;
+                        for (String elem: getSerialize()) {
+                            if (elem == ex)
+                                plugError = false;
+                        }
+
+                    if (plugError) {
+                        JOptionPane.showMessageDialog(oop2GUI.this, "<html><h2>Error</h2>" +
+                                "<i>PlugIn not found</i>");
+                    }
+                    else {
+                        try {
+                            if (plugInOn) {
+                                Class plugClass = Class.forName("com.oop2.plugins." + plugName);
+                                Object plugObject = plugClass.newInstance();
+                                Method method0 = plugClass.getMethod("decode", String.class);
+                                method0.invoke(plugObject, fileOpen.getSelectedFile() + "");
+                                Class serClass = Class.forName("com.oop2.Serialize.Serialize" + exSer.toUpperCase());
+                                Object serObject = serClass.newInstance();
+                                Method method1 = serClass.getMethod("setFilename", String.class);
+                                method1.invoke(serObject, fileOpen.getSelectedFile() + "temp");
+                                Method method2 = serClass.getMethod("getObjects");
+                                elementsNew = (ArrayList<Object>) method2.invoke(serObject);
+                                File file = new File(fileOpen.getSelectedFile() + "temp");
+                                file.delete();
+                                for (Object newObj : elementsNew) {
+                                    Class carClass = newObj.getClass();
+                                    idInfo = carClass.getName() + "|globalId: " +
+                                            Integer.toString(globalId);
+                                    elements.put(idInfo, newObj);
+                                    elementsClone.add(newObj);
+                                    comboBox1.addItem(idInfo);
+
+                                    globalId++;
+                                }
+                            } else {
+                                Class serClass = Class.forName("com.oop2.Serialize.Serialize" + ex.toUpperCase());
+                                Object serObject = serClass.newInstance();
+                                Method method1 = serClass.getMethod("setFilename", String.class);
+                                method1.invoke(serObject, fileOpen.getSelectedFile() + "");
+                                Method method2 = serClass.getMethod("getObjects");
+                                elementsNew = (ArrayList<Object>) method2.invoke(serObject);
+                                for (Object newObj : elementsNew) {
+                                    Class carClass = newObj.getClass();
+                                    idInfo = carClass.getName() + "|globalId: " +
+                                            Integer.toString(globalId);
+                                    elements.put(idInfo, newObj);
+                                    elementsClone.add(newObj);
+                                    comboBox1.addItem(idInfo);
+
+                                    globalId++;
+                                }
+                            }
+                        } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException | InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
--- src/com/oop2/plugins/base32.java
+++ src/com/oop2/plugins/base32.java
@@ -1,13 +1,13 @@
 package com.oop2.plugins;
+
+import org.apache.commons.codec.binary.Base32;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collection;
 
-public class base64 {
+public class base32 {
     public void encode(String filename) throws IOException {
         byte[] toEncode = new byte[20000];
         Path path = Paths.get(filename);
@@ -16,7 +16,8 @@ public class base64 {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        byte[] encoded = Base64.getEncoder().encode(toEncode);
+        Base32 base32 = new Base32();
+        byte[] encoded = base32.encode(toEncode);
         try(FileOutputStream fos = new FileOutputStream(filename, false))
         {
             fos.write(encoded, 0, encoded.length);
@@ -34,7 +35,8 @@ public class base64 {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        byte[] decoded = Base64.getDecoder().decode(toDecode);
+        Base32 base32 = new Base32();
+        byte[] decoded = base32.decode(toDecode);
         try(FileOutputStream fos = new FileOutputStream(filename+"temp", false))
         {
             fos.write(decoded, 0, decoded.length);
